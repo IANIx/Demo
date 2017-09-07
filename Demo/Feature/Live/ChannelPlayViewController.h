8245d55cf2649a000aac89a1c32c48f714f3027a
IANIx <1481231861@qq.com>
2017-09-07 15:20:11 +0800

    request

---
+++ Demo/Feature/Live/ChannelPlayViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChannelPlayViewController.h
+//  Demo
+//
+//  Created by 李勇杰 on 2017/9/7.
+//  Copyright © 2017年 李勇杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChannelPlayViewController : UIViewController
+
+@end
